ec71da3f0711b386873cdd19475a21f0200cdb60
krishnapriyaks400 <krishnapriyaks400@gmail.com>
2023-08-03 09:41:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Favourite from "./pages/Favourite";
+
+import Nav from "./components/Nav";
+import {  useState } from "react";
 function App() {
+  const [contacts, setContacts] = useState([]);
+
+  const formSub =  (data) => {
+    setContacts([...contacts, data]);  
+  }
+
+    const deleteContact = (id) => {
+      let newContact = contacts.filter((singleContact) => {
+      return singleContact.id !== id;
+    });
+ setContacts(newContact);
+  };
+
+  const favToggle =  (id) => {
+    let updatedContact = contacts.map((singleContact) => {
+      return singleContact.id === id
+        ? { ...singleContact, fav: !singleContact.fav }
+        : singleContact;
+    });
+    setContacts(updatedContact);
+  
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Nav/>
+
+    <Routes>
+    
+    <Route exact path ="/" element={<Home
+     formSub={formSub} 
+    contacts={contacts}
+    favToggle={favToggle}
+    deleteContact={deleteContact} />}> </Route>
+
+      <Route path="/favourite" element={<Favourite
+       contacts={contacts}
+       favToggle={favToggle}
+       deleteContact={deleteContact}/>}></Route>
+      
+      </Routes>
+  </BrowserRouter>
   );
 }
 
